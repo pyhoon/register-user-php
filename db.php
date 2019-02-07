88d0f1ea4cf0e893621e1a9a9951f52dc3b1e793
Poon Yip Hoon <yiphoon@yahoo.com>
2019-02-08 05:26:49 +0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+	$host = "mysql16.000webhost.com";	
+	$user = "a1438837_id";
+	$pass = "a1438837";
+	$db = "a1438837_db";
+	$mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
+?>
