5ceb6c090481a50a28bb4efcdcf60dc2991a8a3a
Poon Yip Hoon <yiphoon@yahoo.com>
2019-02-08 05:39:14 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,64 +1,111 @@
 <?php
-$host = "mysql16.000webhost.com";
-$db = "a1438837_db";
-$user = "a1438837_id";
-$pw = "a1438837";
+    require 'db.php';
+    try
+    {
+        if(!isset($_GET['Action']) || empty($_GET['Action']))
+        {
+            print json_encode("Parameter Error");
+            exit;
+        }        
+        $action = $mysqli->escape_string($_GET["Action"]);
 
-$con = mysql_connect($host,$user,$pw) or die(mysql_error());
-mysql_select_db($db) or die(mysql_error());
-mysql_query("SET CHARACTER SET utf8");
-mysql_query("SET NAMES 'utf8'");
+        switch ($action)
+        {
+            case "Register":
+                if (!isset($_GET['UserID']) || empty($_GET['UserID']) || 
+                    !isset($_GET['Email']) || empty($_GET['Email']) || 
+                    !isset($_GET['Password']) || empty($_GET['Password']) || 
+                    !isset($_GET['FullName']) || empty($_GET['FullName']) || 
+                    !isset($_GET['Location']) || empty($_GET['Location']))
+                {
+                    print json_encode("Parameter Error");
+                    exit;
+                }
+                $user = $mysqli->escape_string($_GET["UserID"]);
+                $email = $mysqli->escape_string($_GET["Email"]);
+                $password = $mysqli->escape_string($_GET["Password"]);
+                $fullname = $mysqli->escape_string($_GET["FullName"]);
+                $location = $mysqli->escape_string($_GET["Location"]);
+                $sql = "SELECT * FROM tbl_member";
+                $sql .= " WHERE email = '".$email."'";
+                $sql .= " OR user_id = '".$user."'";
+                $result = $mysqli->query($sql);
+                $count = $result->num_rows;
+                if ($count == 0)
+                {
+                    $randomnumber = mt_rand(111111, 999999);
+                    $sql = "INSERT INTO tbl_member";
+                    $sql .= " (user_id, user_name, pass_word,";
+                    $sql .= " email, location,";
+                    $sql .= " reg_status, reg_no, online)";
+                    $sql .= " VALUES (";
+                    $sql .= " '$user', '$fullname', '$password',";
+                    $sql .= " '$email', '$location',";
+                    $sql .= " 'M', $randomnumber, 'N')";
+                    $mysqli->query($sql);
+                    $to      = $email;
+                    $subject = "B4A Register User Demo";
+                    $message = "Hi ".$user.","."\r\n";
+                    $message .= "Please click on this link to finish";
+                    $message .= " the registration process:";
+                    $message .= " http://demo.computerise.my/b4a/register-user-php/";
+                    $message .= "signup.php?Action=Mail";
+                    $message .= "&Mail=".$email;
+                    $message .= "&RegNo=".$randomnumber;
+                    $message = wordwrap($message, 70, "\r\n");
+                    $headers = "From: mailer@computerise.my\r\n";
+                    $headers .= "Reply-To: mailer@computerise.my\r\n";
+                    $headers .= "X-Mailer: PHP/".phpversion();
 
-$action = $_GET["Action"];
-
-switch ($action) {
-    case "Register":
-        $user = mysql_real_escape_string($_GET["UserID"]);
-        $email = mysql_real_escape_string($_GET["Email"]);
-        $password = mysql_real_escape_string($_GET["Password"]);
-        $fullname = mysql_real_escape_string($_GET["FullName"]);
-        $location = mysql_real_escape_string($_GET["Location"]);
-      
-        $q = mysql_query("SELECT * FROM tbl_member WHERE email = '$email' or user_id = '$user'");
-        $count = mysql_num_rows($q);
-            
-        if ($count == 0)
-            {          
-            $randomnumber = mt_rand(111111, 999999);
-            $res = mysql_query("INSERT INTO tbl_member (user_id, user_name, pass_word, email, location, reg_status, reg_no, online) VALUES ('$user', '$fullname', '$password', '$email', '$location', 'M', $randomnumber, 'N')");
-
-            $to      = $email;
-            $subject = 'Your registration';
-            $message = 'Hi ' . $user . ',' . "\r\n" . 'Please click on this link to finish the registration process: http://kbase.herobo.com/signup.php?Action=Mail&Mail=' . $email . '&RegNo=' . $randomnumber;
-            $message = wordwrap($message, 70, "\r\n");
-            $headers = 'From: register@kbase.herobo.com' . "\r\n" . 'Reply-To: register@kbase.herobo.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-
-            mail($to, $subject, $message, $headers);
-            // Notify me of new sign up
-            mail('register@kbase.herobo.com', 'New member', 'New member (' . $user . ') has signed up using our demo app.', $headers);
-            print json_encode ("Mail");
-            }          
-        else
-            print json_encode ("MailInUse");      
-            break;
-
-    case "Mail":
-        $regno = $_GET["RegNo"];
-        $mail=$_GET["Mail"];
-      
-        $q = mysql_query("SELECT * FROM tbl_member WHERE email = '$mail' and reg_no = $regno and reg_status = 'M'");
-        $count=mysql_num_rows($q);
-      
-        if ($count == 0)
-            {
-            print json_encode ("This registration is not valid / mail address is already registered");
-            }          
-        else {
-            $res=mysql_query("UPDATE tbl_member SET reg_status = 'R' WHERE email = '$mail' and reg_no =$regno");
-            echo("$mail is registered now :-)");
-            }
-        break;
-    default:
-            echo("Unauthorized action! Please use the app to register.");
-}
+                    mail($to, $subject, $message, $headers);
+                    // Notify me of new sign up
+                    $to      = "mailer@computerise.my";
+                    $subject = "New member";
+                    $message = "New member (".$user.") has signed up using our demo app.";
+                    mail($to, $subject, $message, $headers);
+                    print json_encode("Mail");
+                }
+                else
+                    print json_encode("MailInUse");      
+                break;
+            case "Mail":
+                if (!isset($_GET['RegNo']) || empty($_GET['RegNo']) || 
+                    !isset($_GET['Mail']) || empty($_GET['Mail']))
+                {
+                    print json_encode("Parameter Error");
+                    exit;
+                }            
+                $regno = $mysqli->escape_string($_GET["RegNo"]);
+                $mail = $mysqli->escape_string($_GET["Mail"]);
+                $sql = "SELECT *";
+                $sql .= " FROM tbl_member";
+                $sql .= " WHERE email = '$mail'";
+                $sql .= " AND reg_no = $regno";
+                $sql .= " AND reg_status = 'M'";
+                $result = $mysqli->query($sql);
+                $count = $result->num_rows;     
+                if ($count == 0)
+                {
+                    print json_encode("This registration is not valid / email address is already registered");
+                }
+                else
+                {
+                    $sql = "UPDATE tbl_member";
+                    $sql .= " SET reg_status = 'R'";
+                    $sql .= " WHERE email = '$mail'";
+                    $sql .= " AND reg_no = $regno";
+                    $mysqli->query($sql);
+                    echo("$mail is registered now :-)");
+                    print json_encode("Success");
+                }
+                break;
+            default:
+                echo("Unauthorized action! Please use the app to register.");
+        }
+    }
+    catch (Exception $e)
+    {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+        print json_encode("Failed");
+    }       
 ?>
