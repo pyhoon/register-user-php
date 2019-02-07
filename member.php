296318929c6ff114b92351a799a3f2d963502b07
Poon Yip Hoon <yiphoon@yahoo.com>
2019-02-08 05:31:15 +0800

    Update member.php

--- member.php
+++ member.php
@@ -1,28 +1,30 @@
 <?php
-$host = "mysql16.000webhost.com";
-$db = "a1438837_db";
-$user = "a1438837_id";
-$pw = "a1438837";
-
-$con = mysql_connect($host,$user,$pw) or die(mysql_error());
-mysql_select_db($db) or die(mysql_error());
-mysql_query("SET CHARACTER SET utf8");
-mysql_query("SET NAMES 'utf8'");
-     
-$sql = "SELECT user_id, location, online FROM tbl_member ORDER BY id DESC";
-$res = mysql_query($sql);
-
-if (mysql_errno()) {
-    header("HTTP/1.1 500 Internal Server Error");
-    echo $query.'\n';
-    echo mysql_error();
-}
-else
-{
-    $rows = array();
-    while($r = mysql_fetch_assoc($res)) {
-        $rows[] = $r;
-    }
-    print json_encode($rows);
-}
+    require 'db.php';
+    try
+    {    
+		$sql = "SELECT user_id, location, online";
+		$sql .= " FROM tbl_member";
+		$sql .= " ORDER BY id DESC";
+		$result = $mysqli->query($sql);
+		if ($mysqli->errno)
+		{
+    		header("HTTP/1.1 500 Internal Server Error");
+    		echo $sql.'\n';
+    		echo $mysqli->error;
+		}
+		else
+		{
+    		$rows = array();
+    		while ($row = $result->fetch_assoc()) 
+    		{
+        		$rows[] = $row;
+    		}
+    		print json_encode($rows);
+		}
+	}
+    catch (Exception $e)
+    {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+        print json_encode ("Failed");
+    }	
 ?>
