e89fbfdf7472b22fdf28a3ab35f297b94ae8e740
Poon Yip Hoon <yiphoon@yahoo.com>
2019-02-08 18:08:56 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
-	$host = "mysql16.000webhost.com";	
-	$user = "a1438837_id";
-	$pass = "a1438837";
-	$db = "a1438837_db";
+	$host = "localhost";	
+	$user = "root";
+	$pass = "";
+	$db = "demo_b4a";
 	$mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
+
+	// $server = "http://demo.computerise.my/b4a/";
+	$server = "http://192.168.43.191:8000/register-user-php/";
+	$admin = "mailer@computerise.my";
+	$sender = "no-reply@computerise.my";
 ?>
